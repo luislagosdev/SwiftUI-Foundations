d2b8886d9c0bdb78ed100073200f6dacf2543986
LuisLagos <luis.lagos.dev@outlook.com>
2022-11-15 12:04:47 -0600

    Initial Commit

---
+++ UITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITest
+//
+//  Created by Luis Lagos on 15/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITest/UITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestApp.swift
+//  UITest
+//
+//  Created by Luis Lagos on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
